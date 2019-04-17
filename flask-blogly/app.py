cc1f86f74a46f3791850b0291491faf007d0c021
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-04-17 11:24:06 -0700

    added debugtoolbar and made the templates take 2

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,12 +1,16 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, request, render_template
 from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "oh-so-secret"
 
 connect_db(app)
 db.create_all()
+
+debug = DebugToolbarExtension(app)
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -8,3 +8,20 @@ def connect_db(app):
     """Connect to database"""
     db.app = app
     db.init_app(app)
+
+class user(db.Model):
+    """user."""
+
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer,
+                    primary_key=True,
+                    autoincrement=True)
+    first_name = db.Column(db.String(50),
+                    nullable=False,
+                    unique=True)
+    last_name = db.Column(db.String(50),
+                     nullable=False,
+                     unique=True)
+    image_url = db.Column(db.String(50),
+                     nullable=False)
